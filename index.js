fe91bd646e94b95967fda79108e56863ac08e904
Robert Wagner <rwwagner@vt.edu>
2016-04-03 18:01:07 -0400

    Remove vendor styles and import correctly

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ module.exports = {
   name: 'ember-flatpickr',
   included: function(app) {
     this._super.included(app);
-    app.import(this.treePaths.vendor + '/styles/flatpickr.min.css');
   }
 };
