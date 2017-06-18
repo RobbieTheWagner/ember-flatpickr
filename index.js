42c958571813133fe2adf46d920e02f1ba9fad86
Krati Ahuja <kratiahuja@gmail.com>
2017-06-18 15:32:34 -0700

    Prepare for FastBoot 1.0.

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 /* eslint-env node */
 'use strict';
 
+const fastbootTransform = require('fastboot-transform');
+
 module.exports = {
   name: 'ember-flatpickr',
   options: {
     nodeAssets: {
-      flatpickr() {
+      flatpickrJs: function() {
         const localePaths = this.locales.map(locale => `l10n/${locale}.js`)
 
         return {
-          enabled: !process.env.EMBER_CLI_FASTBOOT,
           srcDir: 'dist',
           import: [
-            'flatpickr.js',
+            'flatpickr.js'
+          ].concat(localePaths),
+          processTree: function(input) {
+            return fastbootTransform(input);
+          }
+        };
+      },
+      flatpickCss: function() {
+        return {
+          srcDir: 'dist',
+          import: [
             this.theme || 'flatpickr.css'
-          ].concat(localePaths)
+          ]
         };
       }
     }
   },
+
   included(app) {
     if (app.options && app.options.flatpickr && app.options.flatpickr.theme) {
       this.theme = `themes/${app.options.flatpickr.theme}.css`;
