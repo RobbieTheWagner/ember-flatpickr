b14080faa9967ff171aa89ce698ce8d98946b135
Robert Wagner <rwwagner90@gmail.com>
2018-05-13 13:57:03 -0400

    Relax flatpickr version

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,11 +1,7 @@
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { settings } from 'ember-native-dom-helpers';
 import { start } from 'ember-qunit';
-
-const { APP: { rootElement } } = config;
-settings.rootElement = rootElement || settings.rootElement;
 
 setApplication(Application.create(config.APP));
 
