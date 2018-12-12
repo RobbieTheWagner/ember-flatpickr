67930b037e8e24892e788d61dbf4eb98db239666
Beto Cantu <betocantu93@gmail.com>
2018-12-12 01:28:25 -0600

    find host

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-'use strict';
+"use strict";
 
-const fastbootTransform = require('fastboot-transform');
+const fastbootTransform = require("fastboot-transform");
 
 module.exports = {
-  name: require('./package').name,
+  name: require("./package").name,
   options: {
     nodeAssets: {
       flatpickr() {
         const localePaths = this.locales.map(locale => `l10n/${locale}.js`);
 
         return {
-          srcDir: 'dist',
+          srcDir: "dist",
           import: {
-            include: [
-              'flatpickr.js',
-              this.theme || 'flatpickr.css'
-            ].concat(localePaths),
+            include: ["flatpickr.js", this.theme || "flatpickr.css"].concat(localePaths),
             processTree(tree) {
               return fastbootTransform(tree);
             }
@@ -25,7 +22,22 @@ module.exports = {
     }
   },
 
-  included(app) {
+  included() {
+    let app;
+
+    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
+    // use that.
+    if (typeof this._findHost === "function") {
+      app = this._findHost();
+    } else {
+      // Otherwise, we'll use this implementation borrowed from the _findHost()
+      // method in ember-cli.
+      let current = this;
+      do {
+        app = current.app || app;
+      } while (current.parent.parent && (current = current.parent));
+    }
+
     if (app.options && app.options.flatpickr && app.options.flatpickr.theme) {
       this.theme = `themes/${app.options.flatpickr.theme}.css`;
     }
