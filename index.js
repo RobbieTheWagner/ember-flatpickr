dad210a1f3da9e302b33ada8e99ce6d2ab014c89
Robert Wagner <rwwagner90@gmail.com>
2020-08-04 13:10:12 -0400

    Support no locales (#499)
    * Support no locales
    
    * Refactor to not need node 14

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   options: {
     nodeAssets: {
       flatpickr() {
-        const localePaths = this.locales.map(locale => `l10n/${locale}.js`);
+        const localePaths = Array.isArray(this.locales) ? this.locales.map(locale => `l10n/${locale}.js`) : [];
 
         return {
           srcDir: 'dist',
