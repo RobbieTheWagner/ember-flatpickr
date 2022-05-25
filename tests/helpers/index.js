3048dd84982d634ede09f40a905162f382a61d55
Robert Wagner <rwwagner90@gmail.com>
2022-05-24 23:32:18 -0400

    Update index.js

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,7 +1,7 @@
 import {
   setupApplicationTest as upstreamSetupApplicationTest,
   setupRenderingTest as upstreamSetupRenderingTest,
-  setupTest as upstreamSetupTest,
+  setupTest as upstreamSetupTest
 } from 'ember-qunit';
 
 // This file exists to provide wrappers around ember-qunit's / ember-mocha's
