b6e8cd0359768ad4bdb5cc73db506ec1f8bc17aa
Robert Wagner <rwwagner90@gmail.com>
2022-08-02 08:56:37 -0400

    Require node >= 14, bump deps (#1270)
    * Require node >= 14, bump deps
    
    * Fix lint

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -10,10 +10,6 @@ module.exports = function (environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
       }
     },
 
