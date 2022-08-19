afd0ea71ba594636a3ecdc682016cf57c99f47ae
Robert Wagner <rwwagner90@gmail.com>
2022-08-19 15:39:50 -0400

    Use ember-data 3 with ember 3

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,6 +11,7 @@ module.exports = async function () {
         name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
+            'ember-data': '~3.24.0',
             'ember-source': '~3.24.3'
           }
         }
@@ -19,6 +20,7 @@ module.exports = async function () {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
+            'ember-data': '~3.28.0',
             'ember-source': '~3.28.0'
           }
         }
