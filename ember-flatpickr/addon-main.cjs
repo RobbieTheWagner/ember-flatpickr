91bbe64114adbb24293c9e58b90b8bdac7a5b06b
AmauryD <deflorenne.amaury@gmail.com>
2024-02-13 22:29:20 +0100

    add option to disable initializer's auto-import of flatpickr

--- ember-flatpickr/addon-main.cjs
+++ ember-flatpickr/addon-main.cjs
@@ -2,4 +2,13 @@
 
 const { addonV1Shim } = require("@embroider/addon-shim");
 
-module.exports = addonV1Shim(__dirname);
+module.exports = {
+  ...addonV1Shim(__dirname),
+  options: {
+    "@embroider/macros": {
+      setOwnConfig: {
+        initializeFlatpickr: true,
+      },
+    },
+  }
+};
--- ember-flatpickr/src/initializers/ember-flatpickr.ts
+++ ember-flatpickr/src/initializers/ember-flatpickr.ts
@@ -1,9 +1,19 @@
-import flatpickr from 'flatpickr';
+import { importSync, macroCondition, getOwnConfig } from '@embroider/macros';
+
+/*
+ * This file is only here to initialize the flatpickr registry at the start of the app
+ * This behavior can be disabled by setting `initializeFlatpickr` to `false` in the `@embroider/macros` config. The module will be then tree-shaken away if not used in the app.
+ */
+interface FlatpickrConfig {
+  initializeFlatpickr: boolean;
+}
+
+if (macroCondition(getOwnConfig<FlatpickrConfig>()?.initializeFlatpickr)) {
+  importSync('flatpickr');
+}
 
 export function initialize() {
-  if (window) {
-    window.flatpickr = flatpickr;
-  }
+  // noop
 }
 
 export default {
